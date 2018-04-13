47fe683d786e6ba66458a08e39502832ef24a5a3
Kevin Mikami <mikamikm@gmail.com>
2018-04-13 21:01:07 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,13 @@
 board = [" "," "," ","O","X"," "," "," "," "]
 
 def display_board
-cell = "   |   |   "
+cell_1 = " #{board[0]}  | #{board[1]}  | #{board[2]}  "
+cell_2 = " #{board[3]}  | #{board[4]}  | #{board[5]}  "
+cell_3 = " #{board[6]}  | #{board[7]}  | #{board[8]}  "
 row = "-----------"
-puts cell
+puts cell_1
 puts row
-puts cell
+puts cell_2
 puts row
-puts cell
+puts cell_3
 end
